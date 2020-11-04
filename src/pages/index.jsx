5fb90c19bd56eb280d85a3510a3f75f0ade00c99
APixelVisuals <apixelvisualsofficial@gmail.com>
2020-11-04 02:33:10 -0800

    Added intro section to homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Head from "../components/Head";
+import InlineSVG from "react-inlinesvg";
+import TextLogo from "../../static/assets/text-logo.svg";
 import "./index.scss";
 
-export default () => (
-    <div id="index">
+export default () => {
 
-        <Head
-            title="Illustra"
-            description="Illustra, the object-oriented, high level image processing library for Node.js"
-        />
+    // Text logo animated
+    const [textLogoAnimated, setTextLogoAnimated] = useState(false);
 
-    </div>
-);
+    // On load
+    useEffect(() => {
+
+        // Get text logo animated
+        const thisSetTextLogoAnimated = sessionStorage.getItem("textLogoAnimated");
+
+        // Set text logo animated state
+        setTextLogoAnimated(thisSetTextLogoAnimated);
+
+        // Set text logo animated
+        sessionStorage.setItem("textLogoAnimated", true);
+    }, []);
+
+    return (
+        <div id="index">
+
+            <Head
+                title="Illustra"
+                description="Illustra, the object-oriented, document based, Node.js image processing toolkit"
+            />
+
+            <div className="intro">
+
+                <InlineSVG src={TextLogo} className={textLogoAnimated && "no-animation"} />
+
+                <p className={`description ${textLogoAnimated && "no-animation"}`}>Object-Oriented. <span>Document Based.</span> Node.js Image Processing Toolkit.</p>
+
+            </div>
+
+        </div>
+    );
+};
