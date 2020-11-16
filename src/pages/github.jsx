c37ec8b9770a0293cd07c624012e00a6f368105f
APixelVisuals <apixelvisualsofficial@gmail.com>
2020-11-16 00:13:27 -0800

    Added github redirect page

---
+++ src/pages/github.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Head from "../components/Head";
+
+export default () => (
+    <Head
+        title="GitHub - Illustra"
+        description="Illustra, the object-oriented, document based, Node.js image processing toolkit"
+        redirect="https://github.com/APixelVisuals/illustra"
+    />
+);
